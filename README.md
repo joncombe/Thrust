7d90dfd289be67c89e073c25b40cad7d431a2aaf
Jon Combe <github@joncombe.net>
2023-05-12 07:08:57 +0700

    Replace flash sound with native html audio

---
+++ README.md
@@ -0,0 +1,16 @@
+Thrust is an excellent, one player, multi-directional shooter computer game written in 1986 by a very clever chap in Cambridge, UK called Jeremy Smith. This is my attempt to recreate his work using JavaScript and the html `<canvas>` tag.
+
+I have licensed the code within this repositority under the terms of the MIT License (http://www.opensource.org/licenses/MIT) enabling other users to use the code as they see fit. However I do not own nor have I seen the source code of the original game, not that I would be able to understand it anyway.
+
+This code was written in 2009 and 2010. You can read about this project and find Thrust related links on my personal website: http://joncom.be/experiments/thrust/
+
+Jon Combe<br />
+January 2012
+
+---
+
+2023 update!
+
+My original version used a flash plugin called soundmanager2 to play the in-game audio. Obviously the demise of flash broke my game. I missed it (Thrust, not flash) so I replaced the flash part with native html audio. Bang! Bang!
+
+It is incredibly weird looking at old code that you were proud of at the time, but would do entirely, entirely differently if you were to start again now. I'm glad my `Hungarian notation` days are behind me. :)
--- thrust/classSound.js
+++ thrust/classSound.js
@@ -1,163 +1,150 @@
-// soundmanager config
-soundManager.waitForWindowLoad = true;
-soundManager.debugMode = false;
-soundManager.flashVersion = 9;
-soundManager.useHighPerformance = true;
-soundManager.url = 'http://static.joncom.be/screen/experiments/thrust/soundmanager2/';
-soundManager.onload = function() {
-  oSound = new Sound();
-  oSound.Init();
-  oSound.Cache();
-
-  // cookie handler
-  $.Jookie.Initialise("thrust", 525600);
-
-  var iVolume = $.Jookie.Get("thrust", "iVol") || 50;
-  if (iVolume != 50) {
-    oSound.Volume(iVolume - 50);
-  }
-
-  var bMute = $.Jookie.Get("thrust", "bMute") || false;
-  if (bMute == true) {
-    oSound.ToggleMute( $("div.mute_off") );
-  }
-};
-
 function Sound() {
-  this.oSounds       = {};
-  this.bShield       = false;
-  this.iShieldTimer  = 0;
-  this.iVolume       = 50;
-  this.bMute         = false;
-  this.bThrust       = false;
-  this.iThrustTimer  = 0;
+  this.oSounds = {};
+  this.bShield = false;
+  this.iVolume = 50;
+  this.bMute = false;
+  this.bThrust = false;
 
-  this.Init = function() {
-    this.oSounds.Countdown   = soundManager.createSound({ id: "Countdown",   url: "http://static.joncom.be/screen/experiments/thrust/sounds/countdown.mp3" });
-    this.oSounds.EnemyBullet = soundManager.createSound({ id: "EnemyBullet", url: "http://static.joncom.be/screen/experiments/thrust/sounds/enemy_bullet.mp3" });
-    this.oSounds.Explosion   = soundManager.createSound({ id: "Explosion",   url: "http://static.joncom.be/screen/experiments/thrust/sounds/explosion.mp3" });
-    this.oSounds.NewLife     = soundManager.createSound({ id: "NewLife",     url: "http://static.joncom.be/screen/experiments/thrust/sounds/newlife.mp3" });
-    this.oSounds.Ping        = soundManager.createSound({ id: "Ping",        url: "http://static.joncom.be/screen/experiments/thrust/sounds/ping.mp3" });
-    this.oSounds.Refuel      = soundManager.createSound({ id: "Refuel",      url: "http://static.joncom.be/screen/experiments/thrust/sounds/refuel.mp3" });
-    this.oSounds.Shield      = soundManager.createSound({ id: "Shield",      url: "http://static.joncom.be/screen/experiments/thrust/sounds/shield.mp3" });
-    this.oSounds.ShipBullet  = soundManager.createSound({ id: "ShipBullet",  url: "http://static.joncom.be/screen/experiments/thrust/sounds/ship_bullet.mp3" });
-    this.oSounds.Thrust      = soundManager.createSound({ id: "Thrust",      url: "http://static.joncom.be/screen/experiments/thrust/sounds/thrust.mp3" });
+  this.Init = function () {
+    this.oSounds.Countdown = new Audio("./sounds/countdown.mp3");
+    this.oSounds.EnemyBullet = new Audio("./sounds/enemy_bullet.mp3");
+    this.oSounds.Explosion = new Audio("./sounds/explosion.mp3");
+    this.oSounds.NewLife = new Audio("./sounds/newlife.mp3");
+    this.oSounds.Ping = new Audio("./sounds/ping.mp3");
+    this.oSounds.Refuel = new Audio("./sounds/refuel.mp3");
+    this.oSounds.Shield = new Audio("./sounds/shield.mp3");
+    this.oSounds.ShipBullet = new Audio("./sounds/ship_bullet.mp3");
+    this.oSounds.Thrust = new Audio("./sounds/thrust.mp3");
+
+    this.oSounds.Shield.loop = true;
+    this.oSounds.Thrust.loop = true;
   };
 
-  this.Cache = function() {
-    var aCache = ['Thrust', 'Shield', 'ShipBullet', 'EnemyBullet', 'Explosion', 'Refuel', 'Countdown', 'Ping'];
-    for (var i = 0; i < aCache.length; i++) {
-      this.oSounds[aCache[i]].play({volume:0 });
-    }
-    this.oSounds['Thrust'].stop();
-    this.oSounds['Shield'].stop();
-  };
-
-  this.Play = function(sSound) {
-    if ((this.bMute == false) && (this.iVolume > 0)) {
+  this.Play = function (sSound) {
+    if (this.bMute == false && this.iVolume > 0) {
       switch (sSound) {
         case "Shield":
-          if ((this.bShield != true) && (oGame.iFuel > 0)) {
-            this.oSounds.Shield.play({volume:this.iVolume });
+          if (this.bShield != true && oGame.iFuel > 0) {
+            this.oSounds.Shield.play();
             this.bShield = true;
-            this.iShieldTimer = setInterval(function() {
-              oSound.oSounds.Shield.setPosition(1);
-            }, 6500);
           }
           break;
         case "Thrust":
-          if ((this.bThrust != true) && (oGame.iFuel > 0)) {
-            this.oSounds.Thrust.play({volume:this.iVolume });
+          if (this.bThrust != true && oGame.iFuel > 0) {
+            this.oSounds.Thrust.play();
             this.bThrust = true;
-            this.iThrustTimer = setInterval(function() {
-              oSound.oSounds.Thrust.setPosition(1);
-            }, 8000);
           }
           break;
         default:
-          this.oSounds[sSound].play({volume:this.iVolume });
+          this.oSounds[sSound].pause();
+          this.oSounds[sSound].currentTime = 0;
+          this.oSounds[sSound].play();
       }
     }
   };
 
-  this.Stop = function(sSound) {
+  this.Stop = function (sSound) {
     switch (sSound) {
       case "Shield":
         if (this.bShield == true) {
-          this.oSounds.Shield.stop();
+          this.oSounds.Shield.pause();
+          this.oSounds.Shield.currentTime = 0;
           this.bShield = false;
-          clearInterval(this.iShieldTimer);
         }
         break;
       case "Thrust":
         if (this.bThrust == true) {
-          this.oSounds.Thrust.stop();
+          this.oSounds.Thrust.pause();
+          this.oSounds.Thrust.currentTime = 0;
           this.bThrust = false;
-          clearInterval(this.iThrustTimer);
         }
         break;
     }
   };
 
-  this.ToggleMute = function(oImg) {
+  this.ToggleMute = function (oImg) {
     this.bMute = !this.bMute;
     if (this.bMute == true) {
-        $(oImg).removeClass("mute_off").addClass("mute_on");
-        $("#volume").css({ "background-position": ("0px -24px") });
+      $(oImg).removeClass("mute_off").addClass("mute_on");
+      $("#volume").css({ "background-position": "0px -24px" });
     } else {
-        $(oImg).removeClass("mute_on").addClass("mute_off");
-        this.Volume(0);
+      $(oImg).removeClass("mute_on").addClass("mute_off");
+      this.Volume(0);
     }
     $.Jookie.Set("thrust", "bMute", this.bMute);
   };
 
-  this.Volume = function(iChange) {
+  this.Volume = function (value) {
     // change and sanitize volume level
-    this.iVolume += iChange;
-    if (this.iVolume < 10) { this.iVolume = 0 }
-    if (this.iVolume > 100) { this.iVolume = 100 }
+    this.iVolume = Math.max(0, Math.min(value, 100));
 
     // stop any looping sounds or user will think the volume control doesn't work
-    this.oSounds['Thrust'].stop();
-    this.oSounds['Shield'].stop();
+    this.oSounds["Thrust"].pause();
+    this.oSounds["Thrust"].currentTime = 0;
+    this.oSounds["Shield"].pause();
+    this.oSounds["Shield"].currentTime = 0;
+
+    // update actual volume
+    Object.values(this.oSounds).forEach(
+      (value) => (value.volume = this.iVolume / 100)
+    );
 
     // update display
     var iX = 0;
     var iY = -24;
     switch (this.iVolume) {
-        case 10:
-        case 60:
-            iX = -48;
-            break;
-        case 20:
-        case 70:
-            iX = -96;
-            break;
-        case 30:
-        case 80:
-            iX = -144;
-            break;
-        case 40:
-        case 90:
-            iX = -192;
-            break;
+      case 10:
+      case 60:
+        iX = -48;
+        break;
+      case 20:
+      case 70:
+        iX = -96;
+        break;
+      case 30:
+      case 80:
+        iX = -144;
+        break;
+      case 40:
+      case 90:
+        iX = -192;
+        break;
     }
     if (this.iVolume >= 50) {
-        iY = -36;
-        if (this.iVolume == 100) {
-            iY = -48;
-        }
+      iY = -36;
+      if (this.iVolume == 100) {
+        iY = -48;
+      }
     }
-    $("#volume").css({ "background-position": (iX + "px " + iY + "px") });
+
+    // update ui
+    $("#volume").css("background-position", `${iX}px ${iY}px`);
     $("#thrust_arena").focus();
 
     // if muted, unmute
     if (this.bMute == true) {
-        $("div.mute_on").removeClass("mute_on").addClass("mute_off");
-        this.bMute = false;
+      $("div.mute_on").removeClass("mute_on").addClass("mute_off");
+      this.bMute = false;
     }
 
     // save cookie
     $.Jookie.Set("thrust", "iVol", this.iVolume);
   };
+
+  this.VolumeDown = function () {
+    this.Volume(this.iVolume - 10);
+  };
+
+  this.VolumeUp = function () {
+    this.Volume(this.iVolume + 10);
+  };
+}
+
+var oSound = new Sound();
+oSound.Init();
+$.Jookie.Initialise("thrust", 525600);
+oSound.Volume($.Jookie.Get("thrust", "iVol") || 50);
+var bMute = $.Jookie.Get("thrust", "bMute") || false;
+if (bMute == true) {
+  oSound.ToggleMute($("div.mute_off"));
 }
